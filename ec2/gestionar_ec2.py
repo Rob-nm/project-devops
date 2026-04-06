e15741c6e9fa6fb06acf5dfe4358a87a2662bec2
EC2 Default User <ec2-user@ip-172-31-89-46.ec2.internal>
2026-04-06 14:07:57 +0000

    feat: funciones para listar, iniciar y detener EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -1,17 +1,35 @@
 import boto3
+import sys
 
-# Configuración del cliente para conectarse a EC2
-# Nota: AWS Learner Lab ya tiene las credenciales en el entorno
+# Cliente EC2
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
 def listar_instancias():
-    """Muestra el ID y el estado de las instancias actuales"""
     print("Buscando instancias...")
     response = ec2.describe_instances()
-    
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            print(f"ID: {instance['InstanceId']} - Estado: {instance['State']['Name']}")
+    for res in response['Reservations']:
+        for ins in res['Instances']:
+            print(f"ID: {ins['InstanceId']} - Estado: {ins['State']['Name']}")
+
+def iniciar_instancia(instance_id):
+    print(f"Iniciando instancia {instance_id}...")
+    ec2.start_instances(InstanceIds=[instance_id])
+
+def detener_instancia(instance_id):
+    print(f"Deteniendo instancia {instance_id}...")
+    ec2.stop_instances(InstanceIds=[instance_id])
 
 if __name__ == "__main__":
-    listar_instancias()
+    # Verifica si el usuario pasó argumentos (ej: stop i-123...)
+    if len(sys.argv) > 2:
+        accion = sys.argv[1].lower()
+        id_ins = sys.argv[2]
+        
+        if accion == "start":
+            iniciar_instancia(id_ins)
+        elif accion == "stop":
+            detener_instancia(id_ins)
+        else:
+            print("Acción no válida. Usa 'start' o 'stop'.")
+    else:
+        listar_instancias()
